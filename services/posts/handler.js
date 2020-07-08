33706bc6da8739bb25300b8e85ca950b83a0b66e
Jay <jvelayud@gmail.com>
2020-07-08 13:56:59 -0400

    Testing

--- services/posts/handler.js
+++ services/posts/handler.js
@@ -1,6 +1,14 @@
-export function main(event, context) {
+export const hello = async (event, context) => {
   return {
     statusCode: 200,
-    body: `Hello post!!`,
+    body: JSON.stringify({
+      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
+    }),
   };
-}
+};
+
+const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
+  setTimeout(() => {
+    resolve(`${rest.copy} (with a delay)`);
+  }, time * 1000)
+);
