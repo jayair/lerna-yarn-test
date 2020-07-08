4b0d9b7d7d52c10ca4c91a8f05ebed92ba3f096c
Jay <jvelayud@gmail.com>
2020-07-08 14:03:31 -0400

    fixing handler name

--- services/posts/handler.js
+++ services/posts/handler.js
@@ -1,14 +1,6 @@
-export const main = async (event, context) => {
+export const main = (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
+    body: `Hello post!!`,
   };
 };
-
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
