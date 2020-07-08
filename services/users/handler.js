b496abf73d96190c4fe0f1380103604198837857
Jay <jvelayud@gmail.com>
2020-07-08 16:56:06 -0400

    Adding a root dependency

--- services/users/handler.js
+++ services/users/handler.js
@@ -1,14 +1,13 @@
 import AWS from "aws-sdk";
-import _ from "underscore";
 import logger from "logger";
-import numbers from "../../lib/common";
+import number from "../../lib/common";
 
 AWS.config.update({ region: "us-east-1" });
 
 export async function main(event, context) {
   return {
     statusCode: 200,
-    body: `Hello user #${_.first(numbers())} via ${logger()}`,
+    body: `Hello user #${number()} via ${logger()}`,
   };
 }
 
